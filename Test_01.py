46eb9880285d375ed168575d789dfa636558ef4f
Dobby <ankdanil2003@gmail.com>
2020-03-12 22:56:06 +0700

    Исправление ошибки с генерированием пар и переименование переменных

--- Test_01.py
+++ Test_01.py
@@ -1,22 +1,30 @@
 import random
-list_name = ["Женя", "Никита", "Ваня", "Рома", "Настя", "Миша"]
-kogo_list = []
-d = {}
+
+def selection(name, list_name, busy_list):
+    result_name = random.choice(list_name)
+    while result_name == name or result_name in busy_list:
+        result_name = random.choice(list_name)
+    busy_list.append(result_name)
+    return result_name, busy_list
+
+
+list_name = ["Женя", "Никита", "Ваня", "Рома", "Настя", "Миша"]  # Список всех программистов
+busy_list = []                                                   # Список тех, у кого уже смотрят код
+d = {}                                                           # Словарь кто смотрит - у кого смотрит
 print("Команды:")
 print("!добавить(name), !удалить(name), !рандом")
+
 while True:
-    chat = input()
-    chat_1 = chat.split(' ')
-    if chat_1[0] == "!рандом":
+    chat = input().split(' ')                                     # Что пишет пользователь в строку
+
+    if chat[0] == "!рандом":
         for i in list_name:
-            kogo_list.append(i)
-            c = list(set(list_name)-set(kogo_list))
-            t = random.choice(c)
-            d[i] = t
-            kogo_list.append(t)
-            kogo_list.remove(i)
-        print(d)
-    if chat_1[0] = "!добавить":
-        list_name.append(chat_1[1])
-    if chat_1[0] = "!удалить":
-        list_name.remove(chat_1[1])
+            d[i], busy_list = selection(i, list_name, busy_list)
+        for a, b in d.items():
+            print(a, "→", b)
+
+    if chat[0] == "!добавить":
+        list_name.append(chat[1])
+
+    if chat[0] == "!удалить":
+        list_name.remove(chat[1])
