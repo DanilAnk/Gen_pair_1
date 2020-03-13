a0de70e8a8f1224cca80156c342f378d5120c564
DanilAnk <Ankdanil2003@gmail.com>
2020-03-13 17:43:03 +0700

    work

--- Test_01.py
+++ Test_01.py
@@ -1,30 +1,64 @@
 import random
+import data_name
 
-def selection(name, list_name, busy_list):
+def selection(name, list_name, busy_list, n):
     result_name = random.choice(list_name)
     while result_name == name or result_name in busy_list:
         result_name = random.choice(list_name)
+        if result_name == name:
+            n += 1
+        if n >= 15:
+            break
     busy_list.append(result_name)
-    return result_name, busy_list
+    return result_name, busy_list, n
 
-
-list_name = ["Женя", "Никита", "Ваня", "Рома", "Настя", "Миша"]  # Список всех программистов
+list_name = []
 busy_list = []                                                   # Список тех, у кого уже смотрят код
+can_review_list = []
 d = {}                                                           # Словарь кто смотрит - у кого смотрит
+
+
 print("Команды:")
-print("!добавить(name), !удалить(name), !рандом")
+print("!добавить(name), !удалить(name), !список, !рандом")
+
+for i in list(data_name.dict_name.keys()):
+    if data_name.dict_name[i]["status"] == 1:
+        list_name.append(i)
+    if data_name.dict_name[i]["review"] == 1:
+        can_review_list.append(i)
 
 while True:
     chat = input().split(' ')                                     # Что пишет пользователь в строку
 
     if chat[0] == "!рандом":
-        for i in list_name:
-            d[i], busy_list = selection(i, list_name, busy_list)
+        for i in can_review_list:
+            d[i], busy_list, n = selection(i, list_name, busy_list)
+            if n >= 15:
+                y = random.choice(can_review_list)
+                d[i], d[y] = d[y], d[i]
+        while len(busy_list) != len(list_name):
+            a = random.choice(can_review_list)
+            b, busy_list = selection(a, list_name, busy_list, 0)
+            if n >= 15:
+                y = random.choice(can_review_list)
+                d[i], d[y] = d[y], d[i]
+            d[a] = (d[a], b)
         for a, b in d.items():
             print(a, "→", b)
 
-    if chat[0] == "!добавить":
-        list_name.append(chat[1])
+        busy_list = []
+        d = {}
 
-    if chat[0] == "!удалить":
+    elif chat[0] == "!добавить":
+        list_name.append(chat[1])
+        print(list_name)
+
+    elif chat[0] == "!удалить":
         list_name.remove(chat[1])
+        print(list_name)
+
+    elif chat[0] == "!список":
+        print(list_name)
+
+    else:
+        print("Неправильно")
---
+++ data_name.py
@@ -0,0 +1,9 @@
+dict_name = { "Женя" : { "status" : 1, "review" : 1},
+              "Никита" : { "status" : 1, "review" : 1},
+              "Ваня" : { "status" : 1, "review" : 1},
+              "Рома" : { "status" : 1, "review" : 1},
+              "Настя" : { "status" : 1, "review" : 1},
+              "Миша" : { "status" : 1, "review" : 1},
+              }
+
+lust_d = {}
